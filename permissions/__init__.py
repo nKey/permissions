0f71b3e0f1cb1e60f8881924888a3a447a6a4aaf
Paul Eipper <paul@nkey.com.br>
2014-06-24 20:17:25 -0300

    Add module-level setter method for principals

--- permissions/__init__.py
+++ permissions/__init__.py
@@ -1,3 +1,15 @@
 
 from .permissions import *
 from .roles import *
+
+
+def set_principals(principals):
+    """
+    Use this to set a new Principals dictionary.
+
+    Setting it directly over the main module will not work, since the
+    variable comes from an inner module.
+
+    """
+    from . import permissions
+    permissions.principals = principals
